8ea622a73e665c7fc03a7e41f517cc2a18868e35
Yaser DEMET <ddemety372@gmail.com>
2025-10-11 14:11:15 +0300

    i18n

---
+++ src/i18n/config/index.js
@@ -0,0 +1,9 @@
+import en from '../locales/en/translation.json';
+import tr from '../locales/tr/translation.json';
+
+const resources = {
+  en: { translation: en },
+  tr: { translation: tr }
+};
+
+export default resources;   
