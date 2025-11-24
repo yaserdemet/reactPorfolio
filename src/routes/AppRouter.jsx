daaabb338ad38f96d7130003eaae01ae47b6ef08
Yaser DEMET <ddemety372@gmail.com>
2025-11-24 11:34:15 +0300

    routes

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,24 +1,26 @@
-import React from 'react'
-import { useRoutes } from 'react-router-dom';
-import { HomePage, ProjectPage, AboutPage } from './element';
+import React from "react";
+import { useRoutes, Navigate } from "react-router-dom";
+import { HomePage, ProjectPage, AboutPage } from "./element";
 
 const AppRouter = () => {
-  return (
-    useRoutes([
-        {
-            path: "/",
-            element: <HomePage />
-        },
-        {
-            path: "/project",
-            element: <ProjectPage />
-        },
-        {
-            path: "/about",
-            element: <AboutPage />
-        }
-    ])
-  )
-}
+  return useRoutes([
+    {
+      path: "/",
+      element: <HomePage />,
+    },
+    {
+      path: "/project",
+      element: <ProjectPage />,
+    },
+    {
+      path: "/about",
+      element: <AboutPage />,
+    },
+    {
+      path: "*",
+      element: <Navigate to="/" />,
+    },
+  ]);
+};
 
-export default AppRouter
+export default AppRouter;
